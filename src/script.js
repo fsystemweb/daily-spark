9bcde64d543a0a32756b3bc15b374dad6623ef4d
diazfa <facundoezequiel.diaz@intrum.com>
2024-09-17 12:15:36 +0200

    add animations

--- src/script.js
+++ src/script.js
@@ -1,66 +1,56 @@
-import quotes from './quotes.json'; // Import the JSON file
-
-
+import quotes from './quotes.json';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const phraseContainer = document.getElementById('phraseContainer');
-    const phraseText = document.getElementById('phraseText');
-    const phraseAuthor = document.getElementById('phraseAuthor');
-    const generateButton = document.getElementById('generateButton');
-    const shareButton = document.getElementById('shareButton');
-    const title = document.getElementById('title');
+  const phraseContainer = document.getElementById('phraseContainer');
+  const phraseText = document.getElementById('phraseText');
+  const phraseAuthor = document.getElementById('phraseAuthor');
+  const generateButton = document.getElementById('generateButton');
+  const shareButton = document.getElementById('shareButton');
+  const title = document.getElementById('title');
 
+  phraseContainer.classList.remove('show');
 
-    // Hide the phrase container initially
-    phraseContainer.classList.remove('show');
-    shareButton.classList.add('hidden');
+  function getDayOfYear() {
+    const date = new Date();
+    return Math.floor(date.getTime() / 86400000);
+  }
 
+  const getPhraseIndexForToday = () => {
+    let dayOfYear = getDayOfYear() - 1;
+    dayOfYear = Math.max(1, Math.min(366, dayOfYear));
+    const index = (dayOfYear - 1) % quotes.length + 1;
+    return index;
+  };
 
-    function getDayOfYear() {
-        const date = new Date();
-        return Math.floor(date.getTime() / 86400000);
+  const displayPhrase = () => {
+    generateButton.hidden = true;
+    title.hidden = true;
+    const index = getPhraseIndexForToday();
+    const phrase = quotes[index];
+
+    if (phrase) {
+      const { text, author } = phrase;
+      phraseText.textContent = text;
+      phraseAuthor.textContent = `— ${author}`;
+      phraseContainer.classList.add('show');
+      setTimeout(() => {
+        phraseContainer.classList.add('delayed');
+        shareButton.classList.add('delayed');
+        shareButton.classList.remove('hidden');
+      }, 500);
+    } else {
+      console.error('Phrase is undefined');
     }
+  };
 
-    const getPhraseIndexForToday = () => {
-        let dayOfYear = getDayOfYear() - 1;
-        dayOfYear = Math.max(1, Math.min(366, dayOfYear));
+  generateButton.addEventListener('click', displayPhrase);
 
-        const index = (dayOfYear - 1) % quotes.length + 1;
-        return index;
+  shareButton.addEventListener('click', () => {
+    const shareData = {
+      title: 'Daily Motivation',
+      text: `${phraseText.textContent}\n— ${phraseAuthor.textContent}`,
+      url: window.location.href
     };
-
-
-    const displayPhrase = () => {
-        generateButton.hidden = true;
-        title.hidden = true;
-        const index = getPhraseIndexForToday();
-
-        const phrase = quotes[index];
-
-        if (phrase) {
-            const { text, author } = phrase;
-            phraseText.textContent = text;
-            phraseAuthor.textContent = `— ${author}`;
-
-            phraseContainer.classList.add('show');
-            shareButton.classList.remove('hidden');
-        } else {
-            console.error('Phrase is undefined');
-        }
-    };
-
-    generateButton.addEventListener('click', () => {
-        displayPhrase();
-    });
-
-    shareButton.addEventListener('click', () => {
-        const shareData = {
-            title: 'Daily Motivation',
-            text: `${phraseText.textContent}\n— ${phraseAuthor.textContent}`,
-            url: window.location.href
-        };
-
-        navigator.share ? navigator.share(shareData) : alert('Sharing not supported');
-    });
-
+    navigator.share ? navigator.share(shareData) : alert('Sharing not supported');
+  });
 });
